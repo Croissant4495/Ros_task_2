e67a6c742b94ccc15cb5f39a16220b3da74fec7b
Abdelrahman <abdelrahmanelshabiny@gmail.com>
2023-11-08 21:19:57 +0200

    Now able to get correction angle between pi and -pi

--- src/my_robot_controller/my_robot_controller/turtle_catcher.py
+++ src/my_robot_controller/my_robot_controller/turtle_catcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import math
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -8,9 +9,12 @@ from turtlesim.srv import Spawn
 from functools import partial
 
 class Turtle:
-    def __init__(self, data=None) -> None:
+    def __init__(self, name, data=None) -> None:
         self.name = ""
-        self.pose = Pose()
+        if data:
+            self.pose = data
+        else:
+            self.pose = Pose()
         self.speed = Twist()
     
     def get_attr(self):
@@ -22,26 +26,61 @@ class turtleControl(Node):
         super().__init__("turtle_controller")
         self.speed_publisher = self.create_publisher(Twist, "/turtle1/cmd_vel", 10 )
         self.pose_subscriber = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
-        self.my_turtle = Turtle()
-        self.spawn_new_turtle()
-
+        self.my_turtle = Turtle("base")
+        # self.spawn_new_turtle()
+        # self.spawn_turtles = [random.random()*8 + 1, random.random()*8 + 1, random.random()*8 + 1, random.random() * 2 * 3.14]
+        self.spawn_turtles = [11, -11, random.random() * 2 * 3.14]
+        self.temp = False
 
         self.get_logger().info("Turtle controller started")
 
     def pose_callback(self, pose:Pose):
         self.my_turtle.pose = pose
+        self.main()
+
+    def pub_speed(self):
+        # if turtle near border rotate
         msg = Twist()
-        
-        if pose.x > 9.0 or pose.x < 2.0:
+        if self.my_turtle.pose.x > 9.0 or self.my_turtle.pose.x < 2.0:
             msg.linear.x = 1.0
             msg.angular.z = 0.9
         else:
-            msg.linear.x = 3.0
-            msg.angular.z = 0.0
+            # msg.linear.x = 3.0
+            msg.angular.z = 0.9
+        self.get_logger().info('Current theta: "%f"' % self.my_turtle.pose.theta)
+        self.adjust_angle()
         self.speed_publisher.publish(msg)
         
-        if self.has_collided(pose):
-            self.spawn_new_turtle()
+    def get_correction_angle(self):
+        dy = self.spawn_turtles[1] - self.my_turtle.pose.y
+        dx = self.spawn_turtles[0] - self.my_turtle.pose.x
+        slope_angle = math.atan2(dy, dx)
+        difference = self.my_turtle.pose.theta - slope_angle
+
+        # Adjust differnce to be between pi and -pi
+        if difference > math.pi:
+            difference = difference - 2*math.pi
+        elif difference < -math.pi:
+            difference = difference + 2*math.pi
+
+        self.get_logger().info('Slope angle: "%f"' % slope_angle)
+        self.get_logger().info('Correction angle: "%f"' % (self.my_turtle.pose.theta - slope_angle))
+        return difference
+    
+    def adjust_angle(self):
+        difference = self.get_correction_angle()
+        
+        if abs(difference) < 0.3 or abs(difference) > (2*math.pi - 0.3):
+            # Go forward
+            self.get_logger().info('Forward')
+        elif difference < 0:
+            # Negative go Anti 
+            self.get_logger().info('Anti-clockwise')
+        else:
+            # Positive go clock
+            self.get_logger().info('clockwise')
+
+
 
     def kill_sp_turtle(self):
         pass
@@ -56,8 +95,12 @@ class turtleControl(Node):
         while not client.wait_for_service(1.0):
             self.get_logger().info("Waiting for service.....")
         
+        # create spawn turtle object
+        self.get_logger().info("Craeted client")
+        sp_pose = [random.random()*8 + 1, random.random()*8 + 1, random.random()*8 + 1, * 2 * 3.14]
+        self.sp_turtle = Turtle("spawnee", sp_pose)
+
         # set up request msg
-        self.sp_turtle = Turtle("spawnee", random.random()*8 + 1, random.random()*8 + 1, random.random() * 2*3.14)
         request = Spawn.Request()
         request.name = self.sp_turtle.name
         request.x = self.sp_turtle.x
@@ -76,7 +119,10 @@ class turtleControl(Node):
             self.get_logger().error("Service call failed: %r"%(e,))
 
     def has_collided(self, pose):
-        pass
+        return 0
+
+    def main(self):
+        self.pub_speed()
 
 
 
--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -23,7 +23,8 @@ setup(
             "test_node = my_robot_controller.my_first_node:main",
             "draw_circle = my_robot_controller.draw_circle:main",
             "pose_subscriber = my_robot_controller.pose_subscriber:main",
-            "turtle_controller = my_robot_controller.turtle_border:main"
+            "turtle_border = my_robot_controller.turtle_border:main",
+            "turtle_controller = my_robot_controller.turtle_catcher:main"
         ],
     },
 )
